2178d0470d9ded3af26b7415748dd94710b72727
RafaDaniel <rafadaniel10@gmail.com>
2024-05-23 10:29:19 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,28 @@
+// Variáveis
+let nomeHeroi = ["Thor" , "Mistic" , "Deadpool" , "DrEstranho" , "Homem Aranha"]
+let xpHeroi = [1500, 3000, 7200, 8700, 10001]
+let nivel;
+
+// Estrutura de decisão
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`${nomeHeroi[i]} possui a classificação ${nivel}.`);
+
+
+
